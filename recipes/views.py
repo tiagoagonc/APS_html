88c64aeb39eda394f01a5d7162f1864cefce6817
Tiago Alberto Gonçalves <tiago.agonc@gmail.com>
2022-08-24 17:37:21 -0300

    recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Tiago Gonçalves',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE - Teste APS')
