f5409fadbd596a35aa7aee2dba7a4f81cdf3e82d
Tiago Alberto Gonçalves <tiago.agonc@gmail.com>
2022-09-05 14:38:32 -0300

    page detail

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Tiago Gonçalves',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Tiago Gonçalves',
+    })
